c3c1f25f87d9ee4ae754adc476feb32d6897f561
Esteban Cardona <esteban.cardona1998@gmail.com>
2023-03-21 20:59:55 -0300

    Cambios en App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from React </p>
+        <p>Hello from React. </p>
       </header>
     </div>
   );
